95d60b96f1039e08d6fa888583cfbd4540aa434c
Tobias Tiemerding <tobias@tiemerding.com>
2020-12-27 21:31:52 +0100

    Initial Commit

---
+++ NavigateOnAppear/NavigateOnAppearApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigateOnAppearApp.swift
+//  NavigateOnAppear
+//
+//  Created by Tobias Tiemerding on 27.12.20.
+//
+
+import SwiftUI
+
+@main
+struct NavigateOnAppearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
